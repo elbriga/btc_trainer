ab75008b64d764f618be5b18e2c1ee78d2e209e7
elbriga <gabrielortizlour@gmail.com>
2026-02-04 20:41:09 -0300

    2 USD APIs

--- lib/services/background_service.dart
+++ lib/services/background_service.dart
@@ -39,9 +39,18 @@ class PricesBackgroundService {
       })(),
       (() async {
         try {
-          return await _fetchUsdBrlPrice();
+          // 1st API
+          return await _fetchUsdBrlPrice2();
         } catch (e) {
-          print('=============>>>>>>>>>>> Erro USD: $e');
+          print('=============>>>>>>>>>>> Erro USD2: $e');
+
+          try {
+            // 2nd API backup
+            return await _fetchUsdBrlPrice();
+          } catch (e) {
+            print('=============>>>>>>>>>>> Erro USD1: $e');
+          }
+
           return _currUsdPrice;
         }
       })(),
@@ -50,8 +59,8 @@ class PricesBackgroundService {
     _currBtcPrice = results[0];
     _currUsdPrice = results[1];
 
-    // print('=================>>>>>>>>> CURR BTC $_currBtcPrice');
-    // print('=================>>>>>>>>> CURR USD $_currUsdPrice');
+    print('=================>>>>>>>>> CURR BTC $_currBtcPrice');
+    print('=================>>>>>>>>> CURR USD $_currUsdPrice');
 
     final priceData = PriceData(
       price: _currBtcPrice,
@@ -68,47 +77,56 @@ class PricesBackgroundService {
     });
   }
 
-  Future<http.Response> _fetchHttp(String url) async {
-    int retries = 3;
-    while (retries > 0) {
-      try {
-        return await http.get(Uri.parse(url));
-      } catch (_) {}
-      retries--;
-      await Future.delayed(Duration(seconds: 2));
-    }
-    throw Exception('Falha _fetchHttp($url)');
-  }
-
-  Future<double> _fetchUsdBrlPrice() async {
-    // print('==============>>>>>>>>>>>>>>>> fetchUsdPrice()');
-    // print('==============>>>>>>>>>>>>>>>  fetchUsdPrice()');
-    // print('==============>>>>>>>>>>>>>>>> fetchUsdPrice()');
-    http.Response response = await _fetchHttp(
-      'https://economia.awesomeapi.com.br/json/last/USD-BRL',
-    );
+  Future<http.Response> _fetchHttp(String url, {int timeout = 6}) async {
+    // print('-------=========.>>>>> $url');
+    final response = await http
+        .get(Uri.parse(url))
+        .timeout(
+          Duration(seconds: timeout),
+          onTimeout: () {
+            throw TimeoutException('The connection has timed out');
+          },
+        );
+    // print('-------=== $url = ${response.statusCode}');
 
     if (response.statusCode == 200) {
-      final data = json.decode(response.body);
-      return double.tryParse(data['USDBRL']['high']) ?? 0.00;
+      return response;
     } else {
-      throw Exception('Falha ao carregar o preço do USD-BRL');
+      throw Exception('Failed to load data: ${response.statusCode}');
     }
   }
 
   Future<double> _fetchBtcPrice() async {
+    var url =
+        'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd';
     // print('==============>>>>>>>>>>>>>>>> fetchBtcPrice()');
-    // print('==============>>>>>>>>>>>>>>>  fetchBtcPrice()');
+    // print('=== $url');
     // print('==============>>>>>>>>>>>>>>>> fetchBtcPrice()');
-    http.Response response = await _fetchHttp(
-      'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd',
-    );
 
-    if (response.statusCode == 200) {
-      final data = json.decode(response.body);
-      return data['bitcoin']['usd'].toDouble();
-    } else {
-      throw Exception('Falha ao carregar o preço do BTC');
-    }
+    final response = await _fetchHttp(url);
+    final data = json.decode(response.body);
+    return data['bitcoin']['usd'].toDouble();
+  }
+
+  Future<double> _fetchUsdBrlPrice() async {
+    var url = 'https://economia.awesomeapi.com.br/json/last/USD-BRL';
+    // print('==============>>>>>>>>>>>>>>>> fetchUsdPrice()');
+    // print('=== $url');
+    // print('==============>>>>>>>>>>>>>>>> fetchUsdPrice()');
+
+    final response = await _fetchHttp(url);
+    final data = json.decode(response.body);
+    return double.tryParse(data['USDBRL']['high']) ?? 0.00;
+  }
+
+  Future<double> _fetchUsdBrlPrice2() async {
+    var url = 'https://api.frankfurter.dev/v1/latest?base=USD&symbols=BRL';
+    // print('==============>>>>>>>>>>>>>>>> fetchUsdPrice()');
+    // print('=== $url');
+    // print('==============>>>>>>>>>>>>>>>> fetchUsdPrice()');
+
+    final response = await _fetchHttp(url);
+    final data = json.decode(response.body);
+    return (data['rates']['BRL'] as num).toDouble();
   }
 }
